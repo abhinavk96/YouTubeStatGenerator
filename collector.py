2b1264b77e1dd084b2464da1bc33569878e31069
CosmicCoder96 <abhinav.khare31@gmail.com>
2018-02-26 03:41:28 +0530

    Remove the hardcoded key from the script

--- collector.py
+++ collector.py
@@ -2,7 +2,16 @@ import requests
 import json
 import pprint
 import csv
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 
+dotenv_path = join(dirname(__file__), '.env')
+
+load_dotenv(dotenv_path, verbose=True)
+
+API_KEY = os.environ.get('API_KEY')
+print(API_KEY)
 with open('Law.csv', 'r') as f_in, open('output.csv', 'w') as f_out:
 	cols = ['id', 'views', 'comments']
 	writer = csv.DictWriter(f_out, fieldnames=cols)
@@ -11,7 +20,7 @@ with open('Law.csv', 'r') as f_in, open('output.csv', 'w') as f_out:
 	for row in reader:
 		newRow = {}
 		newRow['id'] = row['id']
-		data = requests.get('https://www.googleapis.com/youtube/v3/videos?id={}&key=AIzaSyABl0yMpz7he4CdOIrvgdNayyMpIu5AXe4&part=snippet,contentDetails,statistics,status'.format(row['id']))
+		data = requests.get('https://www.googleapis.com/youtube/v3/videos?id={}&key={}&part=snippet,contentDetails,statistics,status'.format(row['id'], API_KEY))
 		jsonData=data.json()
 		try:
 			requiredStats=jsonData['items'][0]['statistics']
@@ -22,7 +31,7 @@ with open('Law.csv', 'r') as f_in, open('output.csv', 'w') as f_out:
 		except:
 			print("An unexpected error has occured. API Quota may be over")
 
-#for key in jsonData:
-#	print (key)
-#pprint(dataDict)
+for key in jsonData:
+	print (key)
+pprint(dataDict)
 
